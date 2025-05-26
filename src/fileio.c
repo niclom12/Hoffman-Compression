d23d982cd5b060a0ed9cc3135c34e8704507db81
Peter Couper <25447424@sun.ac.za>
2025-05-26 18:54:11 +0200

    [UPDATE] Encoding and Decoding added

--- src/fileio.c
+++ src/fileio.c
@@ -4,6 +4,8 @@
 #include "fileio.h"
 #include "minheap.h"
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
  * Count character frequencies in a text file
@@ -77,6 +79,81 @@ int write_text_file(const char *filename, const char *text, long length) {
     return 0;
 }
 
+// Adapted from this https://www.geeksforgeeks.org/run-length-encoding/
+char *runLengthEncode(char *buffer, long length)
+{
+    // Written for how many bytes written
+    int runLength, written, j = 0;
+    // Max size (unfortunately)
+    int maxSize = (length * 2 + 1);
+    char *encoded = (char *)malloc(sizeof(char) * maxSize);
+
+    for (size_t i = 0; i < length; i++)
+    {
+        // Copy first occurence
+        encoded[j++] = buffer[i];
+
+        runLength = 1;
+        while (i + 1 < length && buffer[i] == buffer[i + 1])
+        {
+            runLength++;
+            i++;
+        }
+
+        written = snprintf(encoded + j, maxSize - j, "%d", runLength);
+
+        if (written < 0 || written >= maxSize - j)
+        {
+            // Panic -> This should never happen.
+        }
+        else
+        {
+            j += written;
+        }
+    }
+    encoded[j] = '\0';
+    return encoded;
+}
+
+// Adapted from this https://www.geeksforgeeks.org/run-length-encoding/
+char *runLengthDecode(char *buffer, long length)
+{
+    // We assume decoded is at least double and double size each time its bigger
+    // ? We could run through the buffer first to figure out size but I thought this was slightly faster (albeit not space effecient).
+    int decodedCurLength = (length + 1) * 2, repeats, j = 0, i = 0;
+    char *decoded = (char *) malloc(sizeof(char) * decodedCurLength);
+    char currentChar;
+    
+    while (i < length)
+    {
+        currentChar = buffer[i++];
+
+        repeats = buffer[i++] - '0';
+        while (isdigit(buffer[i]))
+        {
+            repeats = repeats * 10 + (buffer[i++] - '0');
+        }
+
+        // Resize (doubling strategy)
+        if (j + repeats >= decodedCurLength) {
+            decodedCurLength *= 2;
+            char *tmp = (char *) malloc(sizeof(char) * decodedCurLength);
+            memcpy(tmp, decoded, j);
+            free(decoded);
+            decoded = tmp;
+        }        
+
+        // A4 -> AAAA
+        for (size_t k = 0; k < repeats; k++)
+        {
+            decoded[j++] = currentChar;
+        }
+    }
+    decoded[j] = '\0';
+
+    return decoded;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Usage: %s <filename>\n", argv[0]);
@@ -104,6 +181,15 @@ int main(int argc, char *argv[]) {
 
     // Call unit tests of min heap
     unit_test();
+
+    // TODO: Handle digits. Like the idea of escape characters the most (other good option is fixed width runs)
+    char *buffer = "AAAaaaBB   BCCCDDDEEEEEeEE!!?!!__-- XYZxXxXxX"
+                   "LLLLLLLLLlllllllllZZZZZzzzzzMMMMMnnnpppppPpPpP";
+    printf("Original version: %s\n", buffer);
+    char *buffer2 = runLengthEncode(buffer, strlen(buffer));
+    printf(" Encoded version: %s\n", buffer2);
+    char *buffer3 = runLengthDecode(buffer2, strlen(buffer2));
+    printf(" Decoded version: %s\n", buffer3);
 
     return 0;
 }
